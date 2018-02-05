0472981f3422f15ff3258453c926e57dc56b03c5
WiseWill <rafael.willian.engcomp@gmail.com>
2018-02-04 23:36:07 -0200

    Preparação da estrutura que deve receber os dados dos livros

--- src/server/server.go
+++ src/server/server.go
@@ -4,7 +4,18 @@ import (
 	"database/sql"
 	"github.com/gorilla/mux"
 	"github.com/go-sql-driver/mysql"
+	"encodin/json"
 )
+
+type Book struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+	Description string `json:"description"`
+	Author string `json:"author"`
+	Publisher string `json:"publisher"`
+	PublishDate string `json:"publishDate"`
+	AddDate string `json:"addDate"`
+}
 
 func main() {
 
