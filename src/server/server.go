9515105957bc58186b470d4665f43e0f48d13772
WiseWill <rafael.willian.engcomp@gmail.com>
2018-02-07 10:35:12 -0200

    Criação do endpoint para post de livro

--- src/server/server.go
+++ src/server/server.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"database/sql"
+	"io/ioutil"
 	"github.com/gorilla/mux"
 	_ "github.com/go-sql-driver/mysql"
 	"encoding/json"
@@ -20,38 +21,37 @@ type Book struct {
 	Publisher string `json:"publisher"`
 }
 
+//Função que checa se houve um erro e faz display da mensagem
+func checkError(err error, message string, w http.ResponseWriter) {
+	if err != nil {
+		log.Println(err)
+		w.Write([]byte(message))
+	}
+}
+
 //Função que faz a requisição de todos os livros no BD
 func getAllBooks(w http.ResponseWriter, r *http.Request) {
 	var books []*Book //Cria um array de livros, a serem transformados em JSON
 
-	rows, err := database.Query("SELECT * FROM book") //Faz a requisição ao banco de dados
+	rows, err_query := database.Query("SELECT * FROM book") //Faz a requisição ao banco de dados
 
 	//Caso haja um erro, as operações não podem continuar
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	checkError(err_query, "Error making request to database", w)
 
 	for rows.Next() { //Percorre cada linha da resposta e insere no array de livros
 		book := new (Book)
 		
-		err = rows.Scan(&book.Id, &book.Name, &book.Description, &book.Author, &book.Publisher) //Lê os campos da resposta
+		err_scan := rows.Scan(&book.Id, &book.Name, &book.Description, &book.Author, &book.Publisher) //Lê os campos da resposta
 
-		if err != nil {
-			log.Println(err)
-			return
-		}
+		checkError(err_scan, "Error scanning rows to type Book", w)
 
 		books = append(books, book) //Insere o livro no array
 	}
 
-	booksJson, err := json.Marshal(books) //Serializa o array em JSON
+	booksJson, err_json := json.Marshal(books) //Serializa o array em JSON
 
 	//Caso haja um erro, como type mismatch, a operação deve ser cancelada
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	checkError(err_json, "Error parsing data type 'Book' to json", w)
 
 	w.Write(booksJson) //Devolve a resposta
 }
@@ -61,47 +61,54 @@ func getSingleBook(w http.ResponseWriter, r *http.Request) {
 	book := new (Book) //Cria um novo livro
 	vars := mux.Vars(r) //Resgata as variáveis enviadas na request
 
-	rows, err := database.Query("SELECT * FROM book WHERE id = " + vars["id"]) //Faz a requisição ao banco de dados
+	rows, err_query := database.Query("SELECT * FROM book WHERE id = " + vars["id"]) //Faz a requisição ao banco de dados
 
-	//Caso haja um erro, as operações não podem continuar
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	checkError(err_query, "Error making request to database", w)
 
 	for rows.Next() {
-		err = rows.Scan(&book.Id, &book.Name, &book.Description, &book.Author, &book.Publisher) //Lê os campos da resposta
+		err_scan := rows.Scan(&book.Id, &book.Name, &book.Description, &book.Author, &book.Publisher) //Lê os campos da resposta
 
-		if err != nil {
-			log.Println(err)
-			return
-		}
+		checkError(err_scan, "Error scanning rows to type Book", w)
 	}
 
-	if book.Id == 0 { //Se o id do livro é igual a zero, quer dizer que não houveram resposta
-		w.Write(nil)
+	if book.Id == 0 { //Se o id do livro é igual a zero, quer dizer que não houveram respostas
+		w.Write([]byte("Não existem livros na base de dados"))
 		return
 	}
 
-	bookJson, err := json.Marshal(book) //Serializa o livro em JSON
+	bookJson, err_json := json.Marshal(book) //Serializa o livro em JSON
 
 	//Caso haja um erro, como type mismatch, a operação deve ser cancelada
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	checkError(err_json, "Error parsing data type 'Book' to json", w)
+
 
 	w.Write(bookJson) //Retorna a resposta
-}
+}  
 
+//Função que insere um livro no banco de dados
 func postSingleBook(w http.ResponseWriter, r *http.Request) {
+	book := new (Book) //Novo livro a ser adicionado
+	body, err_reading := ioutil.ReadAll(r.Body) //Resgata as informações da request
 
+	checkError(err_reading, "Error reading request body.", w) 
+
+	err_json := json.Unmarshal(body, &book) //Transforma as informações da request em tipo Book
+
+	checkError(err_json, "Error parsing body to Json", w)
+
+	statement, err_prepare := database.Prepare("INSERT book SET `name` = ?, `description` = ?, `author` = ?, `publisher` = ?;") //Prepara uma request ao banco de dados
+
+	checkError(err_prepare, "Error preparing insert statement", w) 
+
+	_, err_insert := statement.Exec(book.Name, book.Description, book.Author, book.Publisher) //Executa a inserção
+
+	checkError(err_insert, "Error executing insertion on Database", w)
 }
 
 func main() {
 	router := mux.NewRouter() //Cria um roteador de requisições
 
-	database, _ = sql.Open("mysql", "root:Mercenary.1@/BookSchema") //Cria uma conexão com o banco
+	database, _ = sql.Open("mysql", "root:Rootpass.19@/BookSchema") //Cria uma conexão com o banco
 
 	//Configura-se dois endpoints serem acessados por outras aplicações
 	router.HandleFunc("/books", getAllBooks).Methods("GET") 
